3d531549067222b921b7d9a56ce216e54041e181
Oscar Thunberg <ostenip@gmail.com>
2013-12-08 17:32:51 +0100

    removed debug code

--- src/se/liu/ida/oscth887oskth878/tddc69/project/client/Client.java
+++ src/se/liu/ida/oscth887oskth878/tddc69/project/client/Client.java
@@ -25,6 +25,7 @@ public class Client {
     public static final int UI_SIZE = PIXELS_PER_TILE * 2;
     public static Player player = new Player();
     public static GameClient server;
+    private static final int framerate = 20;
 
     public static void main(String[] args) {
         Renderer renderer = new GLBegin();
@@ -52,9 +53,6 @@ public class Client {
 
         renderer.init(Game.WIDTH* PIXELS_PER_TILE, Game.HEIGHT * PIXELS_PER_TILE + UI_SIZE);
 
-        int frames = 0;
-        long lastTime = System.nanoTime();
-
         Game.init();
 
         while (!Display.isCloseRequested()) {
@@ -65,19 +63,8 @@ public class Client {
             renderer.drawLevel(Game.level);
             renderer.drawUI();
 
-
-            // simple, relatively accurate fps counter
-            /*
-            frames ++;
-            if (System.nanoTime()-lastTime > 1000000000) {
-                System.out.println(frames);
-                frames = 0;
-                lastTime =   System.nanoTime();
-            }
-            */
-
             Display.update();
-            Display.sync(20);
+            Display.sync(framerate);
         }
         server.close("Closing client");
         Display.destroy();
